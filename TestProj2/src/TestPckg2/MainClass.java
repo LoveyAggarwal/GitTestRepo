e9c87a31ef19cfdc4d0dba4a94e87618e8715ab0
Lovey Aggarwal <lovey.saggi@gmail.com>
2019-07-10 13:13:15 +0530

    first ever commit

---
+++ TestProj2/src/TestPckg2/MainClass.java
@@ -0,0 +1,12 @@
+package TestPckg2;
+
+public class MainClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("\n main func for test pckg2");
+
+	}
+
+}
